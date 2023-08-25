2f9e9adf4d6c695df5fd0f3a2f5e4196c19a5fbb
GenericProgrammer1234 <133603260+GenericProgrammer1234@users.noreply.github.com>
2023-08-25 15:26:23 +0000

    General: Updated installer to have branch options

--- install.py
+++ install.py
@@ -5,10 +5,15 @@ print("Do you want to do a system-wide install or user install?")
 print("1. System-wide Install")
 print("2. User Install")
 t = int(input())
+print("Do you want to go with the latest stable version or install the latest development version (NOT RECOMMENDED)")
+print("1. Stable")
+print("2. Dev")
+x = int(input())
+branch = "stabe" if x == 1 else "dev"
 if t == 1:
-	os.system("cd / && git clone https://github.com/GenericProgrammer1234/Pyratch.git")
+	os.system(f"cd / && git clone https://github.com/GenericProgrammer1234/Pyratch.git -b {branch}")
 elif t == 2:
-	os.system("cd ~ && git clone https://github.com/GenericProgrammer1234/Pyratch.git")
+	os.system(f"cd ~ && git clone https://github.com/GenericProgrammer1234/Pyratch.git -b {branch}")
 print("Downloading dependicies")
 os.system("python3 -m pip install scratchattach flask")
 print("Done! Go to " + ("~/Pyratch" if t == 2 else "/Pyratch") + " and then run it!")
