9be6f5da98b7121787dc65424341d47d16bfd94a
GenericProgrammer1234 <133603260+GenericProgrammer1234@users.noreply.github.com>
2023-08-28 10:54:31 +0000

    General: Updated installer to automatically run Pyratch and correct stable branch name

--- install.py
+++ install.py
@@ -1,4 +1,7 @@
 import os
+os.system("alias python=python3")
+os.system("echo 'alias python=python3' >> ~/.bash_profile")
+os.system("echo 'alias python=python3' >> ~/.zshrc")
 print("Pyratch Installer")
 print("Made by AH (@GenericProgrammer1234 on GitHub)")
 print("Do you want to do a system-wide install or user install?")
@@ -9,11 +12,13 @@ print("Do you want to go with the latest stable version or install the latest de
 print("1. Stable")
 print("2. Dev")
 x = int(input())
-branch = "stabe" if x == 1 else "dev"
+branch = "stable" if x == 1 else "dev"
 if t == 1:
 	os.system(f"cd / && git clone https://github.com/GenericProgrammer1234/Pyratch.git -b {branch}")
 elif t == 2:
 	os.system(f"cd ~ && git clone https://github.com/GenericProgrammer1234/Pyratch.git -b {branch}")
 print("Downloading dependicies")
-os.system("python3 -m pip install scratchattach flask")
-print("Done! Go to " + ("~/Pyratch" if t == 2 else "/Pyratch") + " and then run it!")
+os.system("python -m pip install scratchattach flask")
+print("Running Pyratch")
+os.system("cd " + ("~/Pyratch" if t == 2 else "/Pyratch") + " && python install.py")
+print("Done! Now go to localhost:5000 to use Pyratch! In the future you'll need to go to " + ("~/Pyratch" if t == 2 else "/Pyratch") + " and run it!")
