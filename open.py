6c7211a0308095be01f3745c4802d7c9ce2bb091
zayoon013 <125116818+zayoon013@users.noreply.github.com>
2023-08-31 09:47:14 +0000

    fixed simple issue

--- open.py
+++ open.py
@@ -2,5 +2,6 @@ import os
 os.system("alias python=python3")
 os.system("echo 'alias python=python3' >> ~/.bash_profile")
 os.system("echo 'alias python=python3' >> ~/.zshrc")
-print("Done! Now go to https://localhost:5000 to use Pyratch!")
+
 os.system("cd " + ("~/Pyratch" or "/Pyratch") + " && python interface.py")
+print("Done! Now go to https://localhost:5000 to use Pyratch!")
