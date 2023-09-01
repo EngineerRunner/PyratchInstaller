10bd79b52a8c26e864024a0cc6c07a0558e02f9e
zayoon013 <125116818+zayoon013@users.noreply.github.com>
2023-09-01 07:31:21 +0000

    Changed from the or function to asking the user which one did he use

--- open.py
+++ open.py
@@ -2,6 +2,10 @@ import os
 os.system("alias python=python3")
 os.system("echo 'alias python=python3' >> ~/.bash_profile")
 os.system("echo 'alias python=python3' >> ~/.zshrc")
+print("did you choose a systemwide installation or a user installation?")
+print("1. System-wide Install")
+print("2. User Install")
+t = int(input())
+os.system("cd " + ("~/Pyratch" if t == 2 else "/Pyratch") + " && python interface.py")
 
-os.system("cd " + ("~/Pyratch" or "/Pyratch") + " && python interface.py")
 print("Done! Now go to https://localhost:5000 to use Pyratch!")
