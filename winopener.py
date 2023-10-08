207e226742b6ae4f91e6c63ec2da3936cb1190b7
zayoon013 <zain.abudaqqa@icloud.com>
2023-10-08 06:34:58 +0000

    Successfully made the windows opener

---
+++ winopener.py
@@ -0,0 +1,11 @@
+import os
+os.system("DOSKEY python=python3")
+print("Pyratch Installer")
+print("Made by AH (@GenericProgrammer1234 on GitHub)")
+print("Do you want to open a system-wide install or user install?")
+print("1. System-wide Install")
+print("2. User Install")
+t = int(input())
+print("Running Pyratch")
+os.system("cd " + ("C:\\Pyratch" if t == 2 else "%USERPROFILE\\Pyratch") + " && python install.py")
+print("Done! Now go to localhost:5000 to use Pyratch!")
